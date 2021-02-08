79d246c06cc702258f673e050bc9f93c188200a3
lchaconc <luis.chacon.campos@gmail.com>
2021-02-08 08:26:32 -0600

    Reestructuración
    -Instalación de React
    -Reestructuración de plantilla
    -Import Boostrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1>RP GESPRO </h1>
+        <h6>Recursos de Programación GESPRO</h6>
+        <p>
+          Funciones JS y componentes React JS
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
