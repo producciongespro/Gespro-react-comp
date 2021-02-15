ba3b003dcfa3221752403c00b4fe02c7575bce04
lchaconc <luis.chacon.campos@gmail.com>
2021-02-15 08:29:35 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           Funciones JS y componentes React JS
         </p>
       </div>      
-        <ContCalendario />            
+        <ContBotones />            
     </div>
   );
 }
