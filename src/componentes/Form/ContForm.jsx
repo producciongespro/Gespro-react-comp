4593865967789effa95875c2f2c6e8bf75c8986d
lchaconc <luis.chacon.campos@gmail.com>
2021-03-04 08:10:43 -0600

    Mejora en form
    -Se carga un solo json como props con propiedad "control" para determinar que tipo de control renderiza

--- src/componentes/Form/ContForm.jsx
+++ src/componentes/Form/ContForm.jsx
@@ -1,6 +1,7 @@
 import Form from "./Form";
 
 // types: date, number, email, text, range, url
+// control: "input", textArea, select
 
 const inputs = [
   {
@@ -79,12 +80,57 @@ const selects = [
   }
 ];
 
+
+const arrayControls = [
+  {
+    control: "input",
+    id: "nombre",
+    type: "text",
+    label: "Nombre",
+    placeholder: "Escriba su nombre",
+    maxLength: 64,
+    //readOnly: true,
+    //defaultValue: "Pepito",
+    required: true
+  },
+  {
+    control: "input",
+    id: "correo",
+    type: "email",
+    label: "Correo",
+    placeholder: "Debe ser correo mep",
+    maxLength: 20
+    
+  },
+  {
+    control: "textArea",
+    label: "Observaciones",
+    id: "observaciones",
+    rows: "8",
+    required: true
+  },
+  {
+    control: "select",
+    id: "frutas",
+    label:"Frutas",
+    required: true,
+    opts: [{text:"Mango", value: 1 }, {text:"Sandía", value: 2 }, {text:"Piña", value: 3 }     ]
+  },
+  {
+    control: "select",
+    id: "autos",
+    label:"Autos",
+    required: true,
+    opts: [{text:"Toyota", value: 1 }, {text:"Mitsubishi", value: 2 }, {text:"Hyundai", value: 3 }, {text:"Nissan", value: 4 }     ]
+  }
+]
+
 const getDataForm = (data) => {
   console.log("Dataos a enviar al servidor", data);
 };
 
 const ContForm = () => {
-  return <Form getDataForm={getDataForm} inputs={inputs} selects={selects} txtAreas={txtAreas} />;
+  return <Form getDataForm={getDataForm} array={arrayControls} />;
 };
 
 export default ContForm;
--- src/componentes/Form/Form.jsx
+++ src/componentes/Form/Form.jsx
@@ -2,99 +2,112 @@ import { useForm } from "react-hook-form";
 import "./form.css";
 
 const Form = (props) => {
-  const inputs = props.inputs;
-  const txtAreas = props.txtAreas;
-  const selects = props.selects;
   const { register, handleSubmit, errors } = useForm();
   const onSubmit = (data) => {
     props.getDataForm(data);
   };
   console.log(errors);
 
+  const JsxInput = (item, key) => {
+    return (
+      <div className="form-group" key={key}>
+        <label
+          className={`item-negrilla ${errors[item.id] && "item-error"}`}
+          htmlFor={item.id}
+        >
+          {item.required && <span className="item-required">*</span>}
+          {item.label}
+        </label>
+        <input
+          type={item.type}
+          className="form-control"
+          id={item.id}
+          name={item.id}
+          placeholder={item.placeholder}
+          maxLength={item.maxLength}
+          disabled={item.disabled}
+          readOnly={item.readOnly}
+          defaultValue={item.defaultValue && item.defaultValue}
+          ref={register({ required: item.required })}
+        />
+      </div>
+    );
+  };
+
+  const JsxSelect = (item, key) => {
+    return (
+      <div className="input-group mb-3" key={key}>
+        <div className="input-group-prepend">
+          <label
+            className={`input-group-text item-negrilla ${
+              errors[item.id] && "item-error"
+            }`}
+            htmlFor={item.id}
+          >
+            {item.required && <span className="item-required">*</span>}
+            {item.label}
+          </label>
+        </div>
+        <select
+          className="custom-select"
+          name={item.id}
+          id={item.id}
+          ref={register({ required: item.required })}
+        >
+          <option value="">Seleccione una opción</option>
+          {item.opts.map((opt) => (
+            <option key={"opt" + opt.value} value={opt.value}>
+              {opt.text}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  };
+
+  const JsxTxtArea = (item, key) => {
+    return (
+      <div className="input-group mb-4" key={key}>
+        <div className="input-group-prepend">
+          <span
+            // se utilizan string tempale (plantilla de cadena para validar si se agrega la clase error a la etiqueta)
+            className={`input-group-text item-negrilla ${
+              errors[item.id] && "item-error"
+            }`}
+          >
+            {item.required && <span className="item-required">*</span>}
+            {item.label}
+          </span>
+        </div>
+
+        <textarea
+          name={item.id}
+          className="form-control"
+          rows={item.rows}
+          cols={item.cols}
+          aria-label="With textarea"
+          defaultValue={item.defaultValue}
+          disabled={item.disabled}
+          readOnly={item.readOnly}
+          ref={register({ required: item.required })}
+        />
+      </div>
+    );
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {inputs &&
-        inputs.map((item, i) => (
-          <div className="form-group" key={"input" + i}>
-            <label className="item-negrilla" htmlFor={item.id}>
-              {item.label}{" "}
-            </label>
-            <span className="item-error">
-              {errors[item.id] && " Campo requerido"}
-            </span>
-            <input
-              type={item.type}
-              className="form-control"
-              id={item.id}
-              name={item.id}
-              placeholder={item.placeholder}
-              maxLength={item.maxLength}
-              disabled={item.disabled}
-              readOnly={item.readOnly}
-              defaultValue={item.defaultValue && item.defaultValue}
-              ref={register({ required: item.required })}
-            />
-          </div>
-        ))}
-      <br />
-
-      {selects &&
-        selects.map((item, i) => (
-          <div className="input-group mb-3" key={"sel" + i}>
-            <div className="input-group-prepend">
-              <label
-                className={`input-group-text item-negrilla ${
-                  errors[item.id] && "item-error"
-                }`}
-                htmlFor={item.id}
-              >
-                {item.label}
-              </label>
-            </div>
-            <select
-              className="custom-select"
-              name={item.id}
-              id={item.id}
-              ref={register({ required: item.required })}
-            >
-              <option value="">Seleccione una opción</option>
-              {item.opts.map((opt) => (
-                <option key={"opt" + opt.value} value={opt.value}>
-                  {opt.text}
-                </option>
-              ))}
-            </select>
-          </div>
-        ))}
-
-      <br />
-      {txtAreas &&
-        txtAreas.map((item, i) => (
-          <div className="input-group mb-4" key={"txtArea" + i}>
-            <div className="input-group-prepend">
-              <span
-                // se utilizan string tempale (plantilla de cadena para validar si se agrega la clase error a la etiqueta)
-                className={`input-group-text item-negrilla ${
-                  errors[item.id] && "item-error"
-                }`}
-              >
-                {item.label}
-              </span>
-            </div>
-
-            <textarea
-              name={item.id}
-              className="form-control"
-              rows={item.rows}
-              cols={item.cols}
-              aria-label="With textarea"
-              defaultValue={item.defaultValue}
-              disabled={item.disabled}
-              readOnly={item.readOnly}
-              ref={register({ required: item.required })}
-            />
-          </div>
-        ))}
+      {props.array.map((item, i) => {
+        if (item.control === "input") {
+          return JsxInput(item, i);
+        }
+        if (item.control === "select") {
+          return JsxSelect(item, i);
+        }
+        if (item.control === "textArea") {
+          return JsxTxtArea(item, i);
+        }
+      })}
 
       <br />
       <input className="btn btn-outline-info" type="submit" />
