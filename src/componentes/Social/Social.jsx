c48f08d453a1dd787e4017694ad17e2e06930d92
lchaconc <luis.chacon.campos@gmail.com>
2021-04-29 11:03:23 -0600

    Cambio en logica de likes dislikes
    Se cambia por un  estado stateLikes y stateDislikes cada uno se trabaja de forma independiente

--- src/componentes/Social/Social.jsx
+++ src/componentes/Social/Social.jsx
@@ -6,46 +6,69 @@ https://primer.style/octicons/packages/react
 import React, { useState } from "react";
 import { ThumbsupIcon, ThumbsdownIcon, EyeIcon } from "@primer/octicons-react";
 
-
 export default function Social(props) {
   const conf = props.config;
   const item = props.item;
   const [likes, setLikes] = useState(props.item.likes);
   const [enableLike, setEnableLike] = useState(true);
+
   const [dislikes, setDislikes] = useState(props.item.dislikes);
   const [enableDislike, setEnableDislike] = useState(true);
-  const [highlighted, setHighlighted] = useState(null);
+
+  const [stateLike, setStateLike] = useState(false);
+  const [stateDislike, setStateDislike] = useState(false);
 
   const handleLikesDislikes = (e) => {
     const data = {
       id: item.id,
       value: null,
+      state: null,
     };
     //console.log(e.currentTarget.id);
+    //LIKES --------------------------------
     if (e.currentTarget.id === "btnLike" && enableLike) {
-      data.value = "like";
-      setLikes(props.item.likes + 1);
-      setEnableLike(false); 
-      setHighlighted("like");
-      props.putLikesDislikes(data);
-      //Si previamente fue clickeado un dislike:
-      if (!enableDislike) {        
-        setDislikes(dislikes - 1);
+      if (stateLike) {
+        setStateLike(false);
+        setLikes(props.item.likes);
+        data.state = "like";
+        data.value = -1;
+      } else {
+        setStateLike(true);        
+        //coprueba si el dislike fue seleccionado antes
+        if (stateDislike) {
+            setStateDislike(false);
+            //Se resetea el conteo de dislikes a su número original:        
+            setDislikes( props.item.dislikes);
+        }
+
+        setLikes(props.item.likes + 1);
+        data.state = "like";
+        data.value = 1;
       }
-
     }
+
+    //DISLIKES ------------------
     if (e.currentTarget.id === "btnDislike" && enableDislike) {
-      data.value = "dislike";
-      setDislikes(props.item.dislikes + 1);
-      setEnableDislike(false);     
-      setHighlighted("dislike"); 
-      props.putLikesDislikes(data);
-        //Si previamente fue clickeado un like:
-        if (!enableLike) {                    
-            setLikes(likes - 1);
-          }
-
+      if (stateDislike) {
+        setStateDislike(false);
+        setDislikes(props.item.dislikes);
+        data.state = "dislike";
+        data.value = -1;
+      } else {
+        setStateDislike(true);
+        //coprueba si el like fue seleccionado antes
+        if (stateLike) {
+            //Se deshabilita el like
+            setStateLike(false);    
+            //Se resetea el conteo de likes a su número original:        
+            setLikes( props.item.likes);
+        }
+        setDislikes(props.item.dislikes + 1);
+        data.state = "dislike";
+        data.value = 1;
+      }
     }
+    props.putLikesDislikes(data);
   };
 
   return (
@@ -53,7 +76,9 @@ export default function Social(props) {
       <div
         id="btnLike"
         className="col-4 text-center"
-        style={highlighted === "like" ? { backgroundColor: 'yellow', borderRadius:'25%' } : null}
+        style={
+          stateLike ? { backgroundColor: "yellow", borderRadius: "25%" } : null
+        }
         role="button"
         onClick={handleLikesDislikes}
       >
@@ -69,7 +94,11 @@ export default function Social(props) {
       <div
         id="btnDislike"
         className="col-4 text-center"
-        style={highlighted === "dislike" ? { backgroundColor: 'yellow', borderRadius:'25%' } : null}
+        style={
+          stateDislike
+            ? { backgroundColor: "yellow", borderRadius: "25%" }
+            : null
+        }
         role="button"
         onClick={handleLikesDislikes}
       >
